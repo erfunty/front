0a1a657ac77c6baa9234eaad06526c5f4f50518b
erfunty <er12fa34nty56@gmail.com>
2025-12-28 22:00:20 +0330

    Challenge1

---
+++ javascript/js-code/JonasChallenges/Challenge1Scripts.js
@@ -0,0 +1,10 @@
+console.log("Challenge #1");
+const JohnsMass=92;
+const JohnsHeight=1.95;
+const MarksMass=78;
+const MarksHeight=1.69;
+
+const JohnsBMI=JohnsMass/JohnsHeight**2;
+const MarksBMI=MarksMass/MarksHeight**2;
+const markHigherBMI=MarksBMI>JohnsBMI;
+console.log(JohnsBMI,MarksBMI,markHigherBMI);
