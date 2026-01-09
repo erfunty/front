ee461351b096225db36f6ba0a7bb0fc2c6447372
erfunty <er12fa34nty56@gmail.com>
2026-01-09 23:53:32 +0330

    Part2Challenge in video

---
+++ javascript/js-code/JonasChallenges/Part2Challenge3-2Scripts.js
@@ -0,0 +1,11 @@
+`use strict`;
+console.log("Challenge in video");
+
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven']
+};
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friends called ${jonas.friends[0]}`)
