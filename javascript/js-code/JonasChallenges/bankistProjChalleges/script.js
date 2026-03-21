a0c5fdabfc96418416a47876ac43c0180ade5a51
erfunty <er12fa34nty56@gmail.com>
2026-03-21 23:32:30 +0330

    finished bankist app

--- javascript/js-code/JonasChallenges/bankistProjChalleges/script.js
+++ javascript/js-code/JonasChallenges/bankistProjChalleges/script.js
@@ -126,6 +126,20 @@ btnLogin.addEventListener('click',function(e){
 
 
 })
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
 
 btnTransfer.addEventListener('click',function(e){
   e.preventDefault();
