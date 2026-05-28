8f30545a7b5ecc2a24eab634521d39d3bc389402
erfunty <er12fa34nty56@gmail.com>
2026-05-28 23:12:58 +0330

    day counter part 1

--- react/react-code/jonasReactChallenges/Practice-projects/run-practical/src/index.js
+++ react/react-code/jonasReactChallenges/Practice-projects/run-practical/src/index.js
@@ -1,63 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const skills = [
-  { skillName: "html&css", level:'beginner' , backGroundColor: "#ffa500" },
-  { skillName: "javascript", level: 'intermediate', backGroundColor: "#fff200" },
-  { skillName: "react", level:'advanced' , backGroundColor: "#61dafb" },
-];
+
 function App() {
   return (
-    <div className="card">
-      <Avatar />
-      <div className="data">
-        <Intro />
-        {/* Should contain one Skill component
-        for each web dev skill that you have,
-        customized with props */}
-        <SkillList />
-      </div>
+    <div className="Appcounter" >
+      <Counter />
     </div>
   );
 }
-function Avatar() {
-  return <img src="profilePhoto.webp" className="avatar" alt="avatar" />;
-}
-function Intro() {
-  return (
-    <>
-      <h1>erfan sohrabi</h1>
-      <p className="data">
-        front-end developer and student in znj. almost coding ,try in learn love
-        and also english.
-      </p>
-    </>
-  );
-}
-function SkillList() {
-  return <div className="skill-list">
-    {skills.map(skill=><Skill {...skill}/>)}
-  </div>
-}
-function Skill(props){
-  let emoji;
-  switch(props.level){
-    case'beginner': emoji="😏"
-    break;
-    case'intermediate': emoji="🙄"
-    break;
-    case'advanced': emoji="😑"
-    break;
-    default: emoji="❓"
-  }
-  return <div className="skill" style={{backgroundColor:props.backGroundColor}}>
-    <p>{props.skillName}</p>
-    <p>{emoji}</p>
-  
-  </div>
+function Counter(){
+  const [count,setCount]=useState(0)
+  const [step,setStep]=useState(1)
 
+
+  return <div className="counter" >
+    <div className="count">
+    <button onClick={()=>step>1&&setStep(c=>--c)}>-</button>
+    <div>step:{step}</div>
+    <button onClick={()=>step>=0&&setStep(c=>++c)}>+</button>
+    </div>
+    <div className="count">
+    <button onClick={()=>setCount(c=>c-step)}>-</button>
+    <div>Count:{count}</div>
+    <button onClick={()=>setCount(c=>c+step)}>+</button>
+    </div>
+    <DateT countN={count}/>
+  </div>
 }
+function DateT({countN}){
+   let str
+ const date=new Date()
+ str=`${Math.abs(countN)} days ${countN>0?"from today is":"ago was"} ${ new Date(date.getTime()+countN*24*60*60*1000).toDateString()}`
+ if(countN===0) str=`Today is ${date.toDateString()}`
+ 
+  return<p>{str}</p>
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
