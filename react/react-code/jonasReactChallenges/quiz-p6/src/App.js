05ab94e9e84fcbeabf4b946991443650ce1b7d74
erfunty <er12fa34nty56@gmail.com>
2026-06-27 19:24:51 +0330

    start react proj about use reducer

---
+++ react/react-code/jonasReactChallenges/quiz-p6/src/App.js
@@ -0,0 +1,7 @@
+import DateCounter from './DateCounter'
+export default function App(){
+  return <div>
+    <DateCounter/>
+  </div>
+}
+//cd E:\project\react\react-code\jonasReactChallenges\quiz-p6
