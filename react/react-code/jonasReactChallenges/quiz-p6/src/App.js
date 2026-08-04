64120f68fb764d9ffae8682d157c50911fc06bf6
erfunty <er12fa34nty56@gmail.com>
2026-08-04 18:48:30 +0330

    have fake api and summery in reducer

--- react/react-code/jonasReactChallenges/quiz-p6/src/App.js
+++ react/react-code/jonasReactChallenges/quiz-p6/src/App.js
@@ -1,7 +1,45 @@
-import DateCounter from './DateCounter'
-export default function App(){
-  return <div>
-    <DateCounter/>
-  </div>
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+const reducer = function (state, action) {
+  switch (action.type) {
+    case "dataRecived":
+      return {
+        ...state,
+        questions: action.payLoad,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("unknown");
+  }
+};
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecived", payLoad: data }))
+      .catch((err) => dispatch({type:"dataFailed"}));
+  }, []);
+  return (
+    <div>
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>question</p>
+      </Main>
+    </div>
+  );
 }
 //cd E:\project\react\react-code\jonasReactChallenges\quiz-p6
