1f8e820bf62e76661ffaefc4fe00c0e576ec9c69
erfunty <er12fa34nty56@gmail.com>
2026-08-25 18:35:13 +0330

    connecting supabase to proj

--- react/react-code/jonasReactChallenges/wild-oasis/src/pages/Cabins.jsx
+++ react/react-code/jonasReactChallenges/wild-oasis/src/pages/Cabins.jsx
@@ -1,11 +1,21 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCobins";
 
 function Cabins() {
+  useEffect(()=>{
+    getCabins().then(d=>console.log(d))
+
+  },[])
+
+
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img src="https://znxrmnqblefihdehhwvo.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"/>
     </Row>
   );
 }
